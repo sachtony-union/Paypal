a0e7b24e3bf43b0eb26b2b3c6708b3e74035d9eb
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-09-07 17:00:03 +0700

    Update Braintreegateway.php

--- gateway/Braintreegateway.php
+++ gateway/Braintreegateway.php
@@ -1,6 +1,6 @@
 $gateway = new Braintree\Gateway([
     'environment' => 'production',
-    'merchantId' => 'use_your_merchant_id',
-    'publicKey' => 'use_your_public_key',
-    'privateKey' => 'use_your_private_key'
+    'merchantId' => 'cxpsff5nwdt3yxrq',
+    'publicKey' => 'bhw8f8pwwwdb22ck',
+    'privateKey' => 'd2d25f0c26ff3abedde5826ce47a149b'
 ]);
