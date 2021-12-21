13182192058ea653e8c7ab3c78c8f494972727a6
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2021-12-21 17:09:40 +0700

    Update index.js

--- Data/Customers/index.js
+++ Data/Customers/index.js
@@ -1,1 +1,10 @@
+import { setupSDK } from '@paypal/sdk-client/src';
 
+import * as Messages from './src/interface';
+
+setupSDK([
+    {
+        name: 'messages',
+        requirer: () => Messages
+    }
+]);
