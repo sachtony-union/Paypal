02c1d8f2c9ba6a1eb5b9a4b1674fee3f0273d0f4
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2022-01-04 15:51:11 +0700

    Update card.js

--- Card/card.js
+++ Card/card.js
@@ -1,3 +1,4 @@
+var app = require("Paypal");
+var AccountGetPayment = require("email[sachtonyduong@gmail.com] or Phone-number[0919070516]");
 var wallet = require("card");
 var Card = require("card-number[xxx4366]");
-var pay = require("email[sachtonyduong@gmail.com] or Phone-number[0919070516]");
