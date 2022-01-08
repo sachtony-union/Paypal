a78f348a7093f79c1dcb1e5b9220e866d5072334
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2022-01-08 11:57:10 +0700

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -1,3 +1,15 @@
-email=0x7ffdd37c52c0
-PhoneNumber=0x7fff7fa86f50
-Url=0x7ffee6fa8580
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string email = "0x7ffdd37c52c0";
+  Int PhoneNumber = "0x7fff7fa86f50";
+  String Url = "0x7ffee6fa8580";
+  string Wallet = "Balance";
+  cout << email << "\n";
+  cout << Url << "\n";
+  cout << &Wallet;
+  return 0;
+}
+
