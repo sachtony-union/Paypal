4719ba721f02223683f463c945942855274c107c
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2022-01-19 10:42:05 +0700

    Create load.js

---
+++ Simility/load.js
@@ -0,0 +1,10 @@
+<script>
+	var similityContext = {
+		"customer_id": "<uuid>", 
+		"session_id": "<session id>",
+		"user_id": "<user_id>",
+		"zone": "<zone>",
+		"event_types": "<type>"
+	};
+</script>
+<script type="application/javascript" src="https://cdn.simility.com/b.js"></script>
