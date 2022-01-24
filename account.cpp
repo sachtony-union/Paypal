bbe9c824f74b6bfe8d698a89d31ce05f3a52a774
sachtonyonline <87856018+sachtonyonline@users.noreply.github.com>
2022-01-24 10:24:54 +0700

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -6,7 +6,7 @@ int main() {
   string email = "0x7ffdd37c52c0";
   Int PhoneNumber = "0x7fff7fa86f50";
   String Url = "0x7ffee6fa8580";
-  string Wallet = "Balance";
+  string Wallet = "$Wallet";
   cout << email << "\n";
   cout << Url << "\n";
   cout << &Wallet;
